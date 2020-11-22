2316a715674250b6c8e724e7c630fe9cb219da09
boss-space <68944906+boss-space@users.noreply.github.com>
2020-11-22 22:34:45 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,19 +1,19 @@
 import discord
 import random
 from discord.ext import commands
-list=[]
+listok=[]
 TOKEN = 'NzcxOTkzOTI5MjgxMTc1NjAy.X50NXw.apT66sMXaojNSdduBMgTQ0xR9N0'
 bot = commands.Bot(command_prefix='!')
 
 
 @bot.command(pass_context=True)  # разрешаем передавать агрументы
-async def give(ctx, arg):  # создаем асинхронную фунцию бота
+async def give(ctx):  # создаем асинхронную фунцию бота
     await ctx.send(random.choice(e))  # отправляем обратно аргумент
 @bot.command(pass_context=True)
 async def add(ctx, arg):  # создаем асинхронную фунцию бота
     await ctx.send("добавил"+arg)  # отправляем обратно аргумn
     el=arg
-    list.append("el")
+    listok.append("el")
 
 
 bot.run(TOKEN)

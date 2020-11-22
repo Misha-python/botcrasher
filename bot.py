9484707b6815ac416b6f7281f7842434c13bfa94
boss-space <68944906+boss-space@users.noreply.github.com>
2020-11-22 22:17:04 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,6 +9,7 @@ bot = commands.Bot(command_prefix='!')
 @bot.command(pass_context=True)  # разрешаем передавать агрументы
 async def give(ctx, arg):  # создаем асинхронную фунцию бота
     await ctx.send(random.choice(e))  # отправляем обратно аргумент
+@bot.command(pass_context=True)
 async def add(ctx, arg):  # создаем асинхронную фунцию бота
     await ctx.send("добавил"+arg)  # отправляем обратно аргумn
     e.append(arg)

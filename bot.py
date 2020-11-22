92ba13521de419c4fa08ce21a8901edce0745a9b
boss-space <68944906+boss-space@users.noreply.github.com>
2020-11-22 22:02:31 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,14 +1,14 @@
 import discord
 import random
 from discord.ext import commands
-
+e=["1","2",'3',"4"]
 TOKEN = 'NzcxOTkzOTI5MjgxMTc1NjAy.X50NXw.apT66sMXaojNSdduBMgTQ0xR9N0'
 bot = commands.Bot(command_prefix='!')
 
-sp=['1','e','y','r','u','df','y','5','7','s']
+
 @bot.command(pass_context=True)  # разрешаем передавать агрументы
 async def test(ctx, arg):  # создаем асинхронную фунцию бота
-    await ctx.send(random.choice(sp)  # отправляем обратно аргумент
+    await ctx.send(random.choice(e))  # отправляем обратно аргумент
 
 
 bot.run(TOKEN)

4333daffb3faddd0ea4c1571b93b6b2e6e83dd18
boss-space <68944906+boss-space@users.noreply.github.com>
2020-11-23 16:59:59 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,28 +3,28 @@ import random
 from discord.ext import commands
 listok = ["w","d"]
 TOKEN = 'NzcxOTkzOTI5MjgxMTc1NjAy.X50NXw.apT66sMXaojNSdduBMgTQ0xR9N0'
-bot = commands.Bot(command_prefix='!')
+bot = commands.Bot(command_prefix='l!')
 @bot.group(invoke_without_command=True)
 async def give(ctx):  # создаем асинхронную фунцию бота
     await ctx.send('нало')
 @give.command()
 async def minecraft(ctx):
     idd = ctx.message.author.id
-    await ctx.send(idd)
+   
     if idd == 704560097610825828:
         await ctx.author.send(random.choice(listok))  # отправляем обратно аргумент
     else:
-        await ctx.send("не хубка")
+        await ctx.author.send("не хубка")
 @bot.command(pass_context=True)
-async def add(ctx, arg):  # создаем асинхронную фунцию бота
-    await ctx.send("добавил: "+arg)  # отправляем обратно аргумn
-    el=arg
-    listok.append(el)
-    f = open("accs.txt","r+")
-    ctx.send(*f)
-    data = f.read()
-    await ctx.send(data)
-
+async def all(ctx):  # создаем асинхронную фунцию бота
+    idd = ctx.message.author.id
+    
+    if idd == 704560097610825828:
+        await ctx.send(listok)  # отправляем обратно аргумn
+        await ctx.send("вот вся база")
+    else:
+        await ctx.author.send("не хубка")
+   
 
             
        

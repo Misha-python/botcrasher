6f05f57cb82858a6e8f44997c58be346e1bc26f6
boss-space <68944906+boss-space@users.noreply.github.com>
2020-11-23 09:37:57 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,9 +6,9 @@ TOKEN = 'NzcxOTkzOTI5MjgxMTc1NjAy.X50NXw.apT66sMXaojNSdduBMgTQ0xR9N0'
 bot = commands.Bot(command_prefix='!')
 @bot.event
 async def on_ready():
-while true:
-    game = discord.Game("тут твой текст, который будет отображаться в Играет в")
-    await bot.change_presence(status=discord.Status.idle, activity=game)
+    while true:
+        game = discord.Game("тут твой текст, который будет отображаться в Играет в")
+        await bot.change_presence(status=discord.Status.idle, activity=game)
 @bot.command(pass_context=True)  # разрешаем передавать агрументы
 async def give(ctx):  # создаем асинхронную фунцию бота
     await ctx.send(random.choice(e))  # отправляем обратно аргумент

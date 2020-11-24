b2b3c613d8648989bed4ec007065b2bce3de8656
boss-space <68944906+boss-space@users.noreply.github.com>
2020-11-24 11:18:20 +0300

    Gg

--- bot.py
+++ bot.py
@@ -1,7 +1,24 @@
 import discord
 import random
 from discord.ext import commands
-listok = ["w","d"]
+text='''
+chandlerbarton04@gmail.com:Barton2004!
+thomasjc@live.ca:16tHe19987
+marcosg_94@hotmail.com:marcos1794
+carisido1@aol.com:Cooper76!
+kevinmadsen01@gmail.com:Streetbasket2002
+loganp326@gmail.com:Slendy15
+kierstin.sada1@gmail.com:chickentaters21
+mathes.lol1@web.de:Dusty005005!
+mayamesriani@yahoo.com:Mayamaya2
+j.a.m.r.marceau@gmail.com:Legionasmany1
+killerwaterbug@gmail.com:Sounders23!
+laylalalovelyllama@yahoo.com:disneyfan6
+tcbohls@live.com:Window99!
+kingturtle02@gmail.com:smoothie02
+legendxmachina@gmail.com:TV052804*$
+'''
+listok = []
 TOKEN = 'NzcxOTkzOTI5MjgxMTc1NjAy.X50NXw.apT66sMXaojNSdduBMgTQ0xR9N0'
 bot = commands.Bot(command_prefix='l!')
 @bot.event
